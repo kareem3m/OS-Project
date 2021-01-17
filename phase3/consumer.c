3da0be01e0d8d9b51859af8f7bbc5208cc99a3b4
Dai Alaa <dai.a.elrihany@gmail.com>
2021-01-17 05:53:12 +0200

    modify phase3

--- phase3/consumer.c
+++ phase3/consumer.c
@@ -9,12 +9,6 @@
 #include <signal.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-struct msgbuff
-{
-    long mtype;
-    char mtext[256];
-};
-int buffsize =3;
 
 
 /* arg for semctl system calls. */
@@ -26,9 +20,10 @@ union Semun
     struct seminfo *__buf; /* buffer for IPC_INFO */
     void *__pad;
 };
-int msgq_id_down, msgq_id_up;
 
+int buffsize =3;
 int *shmaddr;
+
 void down(int sem)
 {
     struct sembuf p_op;
@@ -57,115 +52,98 @@ void up(int sem)
         perror("Error in up()");
         exit(-1);
     }
-
 }
-
-
 
 int main(int argc, char** argv)
 { 
-    key_t key_id_sem_full, key_id_sem_empty, key_id_sem_mutex, key_id_sem_index;
+    key_t keyIdSemFull, keyIdSemEmpty, keyIdSemMutex, keyIdSemIndex;
     union Semun semun;
 
-    key_id_sem_full = ftok("keyfile", 1);
-    key_id_sem_empty = ftok("keyfile", 2);
-    key_id_sem_mutex = ftok("keyfile", 3);
-    key_id_sem_index = ftok("keyfile", 5);
-    int full = semget(key_id_sem_full, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int empty = semget(key_id_sem_empty, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int mutex_lock = semget(key_id_sem_mutex, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int index = semget(key_id_sem_index, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    keyIdSemFull = ftok("keyfile", 1);
+    keyIdSemEmpty = ftok("keyfile", 2);
+    keyIdSemMutex = ftok("keyfile", 3);
+    keyIdSemIndex = ftok("keyfile", 5);
+    int full = semget(keyIdSemFull, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    int empty = semget(keyIdSemEmpty, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    int mutexLock = semget(keyIdSemMutex, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    // the index of the buffer where the consumer will consume a process 
+    int index = semget(keyIdSemIndex, 1, 0666 | IPC_CREAT | IPC_EXCL);
 
-    if(full!=-1||empty!=-1|mutex_lock!=-1){
-            semun.val = 0; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(full, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
-            semun.val = 1; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(mutex_lock, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
-            semun.val = buffsize; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(empty, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
+    if(full!=-1||empty!=-1|mutexLock!=-1)
+    {
+        semun.val = 0; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(full, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+        semun.val = 1; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(mutexLock, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+        semun.val = buffsize; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(empty, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
     }
-    else{
-            full = semget(key_id_sem_full, 1, 0666 | IPC_CREAT);
-            empty = semget(key_id_sem_empty, 1, 0666 | IPC_CREAT);
-            mutex_lock = semget(key_id_sem_mutex, 1, 0666 | IPC_CREAT);
+    else
+    {
+        full = semget(keyIdSemFull, 1, 0666 | IPC_CREAT);
+        empty = semget(keyIdSemEmpty, 1, 0666 | IPC_CREAT);
+        mutexLock = semget(keyIdSemMutex, 1, 0666 | IPC_CREAT);
     }
-    if(index != -1){
+
+    // for multi consumers if the index is already created it will take its current value otherwise it will create index
+    if(index != -1)
+    {
         semun.val = 0; /* initial value of the semaphore, Binary semaphore */
         if (semctl(index, 0, SETVAL, semun) == -1)
         {
             perror("Error in semctl");
             exit(-1);
         }
-       
     }
     else
     {
-        index = semget(key_id_sem_index, 1, 0666 | IPC_CREAT);
+        index = semget(keyIdSemIndex, 1, 0666 | IPC_CREAT);
     }
     
-
+    //shared memory initialization
     int shmid;
-    key_t key_id_shm;
-    key_id_shm = ftok("keyfile", 6);
-
-    shmid = shmget(key_id_shm, buffsize*sizeof(int), IPC_CREAT | 0666);
+    key_t keyIdShm;
+    keyIdShm = ftok("keyfile", 6);
+    shmid = shmget(keyIdShm, buffsize*sizeof(int), IPC_CREAT | 0666);
     shmaddr = (int*)shmat(shmid, (void *)0, 0);
+    
     if (shmaddr == -1)
     {
         perror("Error in attach in reader");
         exit(-1);
     }
-    printf("\nReader: Shared memory attached at address %x\n", shmaddr);
-
-
-    key_t key_id_msq;
-
-    //message queue initialization
-    key_id_msq = ftok("keyfile", 6);              
-    int msgq_id = msgget(key_id_msq, 0666 | IPC_CREAT);
-    if (msgq_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
-    }
-    printf("Message Queue ID down = %d\n", msgq_id);
-    
-    struct msgbuff message;
-    int rec_val, send_val;
-    message.mtype = getpid()%10000; /* arbitrary value */
-    strcpy(message.mtext,"producer it's time to wake up");
 
     int i=2;
-    // int rem=0;
     while(1)
     {
+        down(full);  
+        down(mutexLock);
+        i=semctl(index, 0, GETVAL, semun);
+        up(index);
 
-	        down(full);  
-            down(mutex_lock);
-            i=semctl(index, 0, GETVAL, semun);
-            up(index);
-            if(i==buffsize-1){
-                semun.val=0;
-                semctl(index, 0, SETVAL, semun);
-            }
-            printf("%d: consumer has consumed an item\n",shmaddr[i]);
-            // rem=(rem+1)%buffsize;
-            up(mutex_lock);
-            up(empty);
-            sleep(7);
+        // if the index reach buffer size it will wraparound again
+        if(i==buffsize-1)
+        {
+            semun.val=0;
+            semctl(index, 0, SETVAL, semun);
+        }
+
+        printf("%d: consumer has consumed an item\n",shmaddr[i]);
+        up(mutexLock);
+        up(empty);
+        sleep(5);
     }
-
     return 0;
 }
--- phase3/producer.c
+++ phase3/producer.c
@@ -9,12 +9,6 @@
 #include <signal.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
-struct msgbuff
-{
-    long mtype;
-    char mtext[256];
-};
-int buffsize =3;
 
 
 /* arg for semctl system calls. */
@@ -26,9 +20,10 @@ union Semun
     struct seminfo *__buf; /* buffer for IPC_INFO */
     void *__pad;
 };
-int msgq_id_down, msgq_id_up;
 
+int buffsize =3;
 int *shmaddr;
+
 void down(int sem)
 {
     struct sembuf p_op;
@@ -57,117 +52,98 @@ void up(int sem)
         perror("Error in up()");
         exit(-1);
     }
-
 }
-
-
 
 int main(int argc, char** argv)
 { 
-    key_t key_id_sem_full, key_id_sem_empty, key_id_sem_mutex, key_id_sem_index;
+    key_t keyIdSemFull, keyIdSemEmpty, keyIdSemMutex, keyIdSemIndex;
     union Semun semun;
 
-    key_id_sem_full = ftok("keyfile", 1);
-    key_id_sem_empty = ftok("keyfile", 2);
-    key_id_sem_mutex = ftok("keyfile", 3);
-    key_id_sem_index = ftok("keyfile", 4);
-    int full = semget(key_id_sem_full, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int empty = semget(key_id_sem_empty, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int mutex_lock = semget(key_id_sem_mutex, 1, 0666 | IPC_CREAT | IPC_EXCL);
-    int index = semget(key_id_sem_index, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    keyIdSemFull = ftok("keyfile", 1);
+    keyIdSemEmpty = ftok("keyfile", 2);
+    keyIdSemMutex = ftok("keyfile", 3);
+    keyIdSemIndex = ftok("keyfile", 4);
+    int full = semget(keyIdSemFull, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    int empty = semget(keyIdSemEmpty, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    int mutexLock = semget(keyIdSemMutex, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    // the index of the buffer where the producer will put the current process 
+    int index = semget(keyIdSemIndex, 1, 0666 | IPC_CREAT | IPC_EXCL);
 
-    if(full!=-1||empty!=-1||mutex_lock!=-1){
-            semun.val = 0; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(full, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
-            semun.val = 1; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(mutex_lock, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
-            semun.val = buffsize; /* initial value of the semaphore, Binary semaphore */
-            if (semctl(empty, 0, SETVAL, semun) == -1)
-            {
-                perror("Error in semctl");
-                exit(-1);
-            }
+    if(full!=-1||empty!=-1||mutexLock!=-1)
+    {
+        semun.val = 0; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(full, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+        semun.val = 1; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(mutexLock, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
+        semun.val = buffsize; /* initial value of the semaphore, Binary semaphore */
+        if (semctl(empty, 0, SETVAL, semun) == -1)
+        {
+            perror("Error in semctl");
+            exit(-1);
+        }
     }
-    else{
-            full = semget(key_id_sem_full, 1, 0666 | IPC_CREAT);
-            empty = semget(key_id_sem_empty, 1, 0666 | IPC_CREAT);
-            mutex_lock = semget(key_id_sem_mutex, 1, 0666 | IPC_CREAT);
+    else
+    {
+        full = semget(keyIdSemFull, 1, 0666 | IPC_CREAT);
+        empty = semget(keyIdSemEmpty, 1, 0666 | IPC_CREAT);
+        mutexLock = semget(keyIdSemMutex, 1, 0666 | IPC_CREAT);
     }
 
-
-   if(index != -1){
+    // for multi producers if the index is already created it will take its current value otherwise it will create index
+    if(index != -1)
+    {
         semun.val = 0; /* initial value of the semaphore, Binary semaphore */
         if (semctl(index, 0, SETVAL, semun) == -1)
         {
             perror("Error in semctl");
             exit(-1);
-        }
-       
+        } 
     }
     else
     {
-        index = semget(key_id_sem_index, 1, 0666 | IPC_CREAT);
+        index = semget(keyIdSemIndex, 1, 0666 | IPC_CREAT);
     }
 
+    //shared memory initialization
     int shmid;
-    key_t key_id_shm;
-    key_id_shm = ftok("keyfile", 6);
-
-    shmid = shmget(key_id_shm, buffsize*sizeof(int), IPC_CREAT | 0666);
+    key_t keyIdShm;
+    keyIdShm = ftok("keyfile", 6);
+    shmid = shmget(keyIdShm, buffsize*sizeof(int), IPC_CREAT | 0666);
     shmaddr = (int*)shmat(shmid, (void *)0, 0);
+
     if (shmaddr == -1)
     {
         perror("Error in attach in reader");
         exit(-1);
     }
-    printf("\nReader: Shared memory attached at address %x\n", shmaddr);
+  
+    while(1)
+    {       
+        down(empty);          
+        down(mutexLock);
+        i=semctl(index, 0, GETVAL, semun);
+        up(index);
 
+        // if the index reach buffer size it will wraparound again
+        if(i==buffsize-1)
+        {
+            semun.val=0;
+            semctl(index, 0, SETVAL, semun);
+        }
 
-    key_t key_id_msq;
-
-    //message queue initialization
-    key_id_msq = ftok("keyfile", 6);              
-    int msgq_id = msgget(key_id_msq, 0666 | IPC_CREAT);
-    if (msgq_id == -1)
-    {
-        perror("Error in create");
-        exit(-1);
-    }
-    printf("Message Queue ID down = %d\n", msgq_id);
-    
-    struct msgbuff message;
-    int rec_val, send_val;
-    message.mtype = getpid()%10000; /* arbitrary value */
-    int i=0;
-    int add=100;
-    int size;
-    strcpy(message.mtext,"consumer it's time to wake up");
-    //int array[]={1,2,3};
-    while(1){
-                
-                down(empty);          
-                down(mutex_lock);
-                i=semctl(index, 0, GETVAL, semun);
-                up(index);
-                if(i==buffsize-1){
-                    semun.val=0;
-                    semctl(index, 0, SETVAL, semun);
-                }
-                printf("%d: producer has produced an item\n",add);
-                up(mutex_lock);
-	            shmaddr[i]=add;
-                add=add+1;
-                up(full);
-
-
+        printf("%d: producer has produced an item\n",add);
+        up(mutexLock);
+        shmaddr[i]=add;
+        add=add+1;
+        up(full);
     }
     return 0;
 }
